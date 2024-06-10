330276a173205395351f56dd503ac7f4f1fb5ba7
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-06-10 22:30:02 +0600

    getting all users functiionality added

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 5000
 
 
 // middlewares
-app.use(cors())
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+    ]
+}))
 app.use(express.json())
 
 
@@ -85,6 +89,12 @@ async function run() {
             }
             const role = await usersCollection.findOne(query, options)
             res.send(role)
+        })
+
+        app.get('/users/role/tutors', async(req, res) => {
+            const query = {role: "Tutor"}
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
         })
 
 
@@ -177,10 +187,31 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/materials', async(req, res)=> {
+            const result = await materialsCollection.find().toArray()
+            res.send(result)
+        })
+
         app.get('/materials/:email', async (req, res) => {
             const email = req.params.email
             console.log(email)
             const query = { tutorEmail: email }
+            const result = await materialsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.delete('/materials/:id', async(req, res)=> {
+            const id = req.params.id
+            // console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await materialsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.get('/materials/student/:id', async(req, res)=> {
+            const id = req.params.id
+            // console.log(id)
+            const query = { sessionId: id }
             const result = await materialsCollection.find(query).toArray()
             res.send(result)
         })
