436d76580c4a5421f4c6e71462f25c049a635cd8
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-12-22 21:05:54 +0600

    updated database.js

--- src/Config/database.js
+++ src/Config/database.js
@@ -2,8 +2,8 @@
 // mongodb connection starts here
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()  //required to get .env variable's value
-const user = hasibulStart
-const password = hasibulSuchi
+const user = "hasibulStart"
+const password = "hasibulSuchi"
 // console.log("user and password", user, password)
 
 // getting mongdb uri
