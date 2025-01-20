2f3635d3007c700622f8c600167c3e648ff13914
hasibul islam <hasibulislamha01@gmail.com>
2025-01-20 22:31:20 +0600

    added revenue routes

--- src/routes/bookedSessions.js
+++ src/routes/bookedSessions.js
@@ -16,7 +16,7 @@ bookedSessionRoutes.post('/', async (req, res) => {
 })
 
 
-// fetching booked sessions (all, specific student)
+// fetching booked sessions (all, specific student, specific tutor)
 bookedSessionRoutes.get('/', async (req, res) => {
 
     const { studentEmail, tutorEmail } = req.query
@@ -43,6 +43,27 @@ bookedSessionRoutes.get('/counts', async (req, res) => {
         res.status(200).json({ query: studentEmail || tutorEmail, count: result });
     } catch (error) {
         res.status(500).send({ message: "failed to fetch the booked sessions for the student", error })
+    }
+})
+
+// getting revenue count
+bookedSessionRoutes.get('/revenue', async (req, res) => {
+    try {
+        const { tutorEmail } = req.query
+        let query = {}
+        if (tutorEmail) query.tutorEmail = tutorEmail
+
+        const bookedSessions = await bookedSessionCollection.find(query).toArray()
+        // console.log(bookedSessions);
+        let totalFee = 0
+        bookedSessions?.forEach(session => {
+            const sessionFee = parseInt(session.registrationFee)
+            totalFee += sessionFee
+            console.log(totalFee);
+        })
+        res.send({totalRevenue: totalFee, query})
+    } catch (error) {
+        res.status(500).send({ message: 'failed to get revenue value', error })
     }
 })
 
